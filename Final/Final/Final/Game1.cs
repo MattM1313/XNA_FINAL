ed85e3587d112934b6e813d8d78e8414f18bba56
Matt Mayo <matt.mayo1313@gmail.com>
2013-04-04 16:40:15 -0400

    Added Icon Description to Shop, added Enemies, more icons

--- Final/Final/Final/Game1.cs
+++ Final/Final/Final/Game1.cs
@@ -19,17 +19,17 @@ namespace Final
     /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
+
+        #region Variables
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         MouseState prevMouseState;
         KeyboardState prevKeyState;
+        SpriteFont font;
 
         ParticleEngine particleEngine;
-
-
         ShopIcon laserIcon1, laserIcon2, laserIcon3, laserIcon4, laserIcon1G, laserIcon2G, laserIcon3G, laserIcon4G;
-
-
+        ShopIcon shield1, shield2, shield3, shield4;
         enum GameState
         {
             mainMenu,
@@ -46,11 +46,7 @@ namespace Final
         }
         laserState LaserState = laserState.One;
 
-
         mButton btnPlay, btnOptions, btnShop;
-
-
-      
         MultiBackground b;
         Sprite player, bg, gui;
         Texture2D playerTex, back, laser1;
@@ -58,13 +54,14 @@ namespace Final
         Texture2D cursor;
         Vector2 cursorPos;
         List<Sprite> laserList = new List<Sprite>();
+        //List<SpriteFromSheet> enemyList = new List<SpriteFromSheet>();
+        SpriteFromSheet enemy1;
+        SpriteFromSheet enemy2;
 
         const float FIRE_DELAY = 150f;
         float fireDelay = FIRE_DELAY;
-        
-
         float angle;
-
+        #endregion
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -83,12 +80,22 @@ namespace Final
         /// </summary>
         protected override void Initialize()
         {
-
+            
             playerTex = Content.Load<Texture2D>(@"Ship_Sprites/ship01");
             //back = Content.Load<Texture2D>("Background");
             back = Content.Load<Texture2D>("bg");
             player = new Sprite(playerTex, new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2), new Vector2(5, 5),
                 true, 0, 1f, SpriteEffects.None, null, 0);
+            
+            
+            enemy1 = new SpriteFromSheet(Content.Load<Texture2D>("Ship_Sprites/Enemy/Ship_1_Sheet"), new Vector2(100, 100), new Vector2(0, 0), true, 0,
+                0.4f, SpriteEffects.None, null, 0, new Vector2(92, 106), new Vector2(0, 0), new Vector2(2, 1), 0.1f);
+            
+            
+            
+            enemy2 = new SpriteFromSheet(Content.Load<Texture2D>("Ship_Sprites/Enemy/Ship_2_Sheet"), new Vector2(600, 100), new Vector2(0, 0), true, 0,
+                0.4f, SpriteEffects.None, null, 0, new Vector2(90, 106), new Vector2(0, 0), new Vector2(2, 1), 0.1f);
+
             bg  = new Sprite(back, new Vector2(0, 0), new Vector2(0, 0), true, 0, 1f, SpriteEffects.None, null, 0);
             base.Initialize();
 
@@ -105,59 +112,58 @@ namespace Final
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-
-
-
-       
-
-
-
+            font = Content.Load<SpriteFont>("Font");
+            #region particles
             List<Texture2D> textures = new List<Texture2D>();
             textures.Add(Content.Load<Texture2D>("starSmall"));
             particleEngine = new ParticleEngine(textures, new Vector2(400, 240));
+            #endregion
 
+            #region lasers&GUI
             gui = new Sprite(Content.Load<Texture2D>("shopGUI"), new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2), Vector2.Zero, true,
                 0f, 1f, SpriteEffects.None, null, 0);
 
-
-           
-
             laser1 = Content.Load<Texture2D>(@"Ship_sprites/lasers/laser1");
             laser01 = Content.Load<Texture2D>(@"Ship_sprites/lasers/laser01");
+            #endregion
 
+            #region buttons
             btnPlay = new mButton(Content.Load<Texture2D>("play_button"), graphics.GraphicsDevice);
             btnPlay.setPosition(new Vector2(graphics.PreferredBackBufferWidth/2 -125, 300));
             btnOptions = new mButton(Content.Load<Texture2D>("options_button"), graphics.GraphicsDevice);
             btnOptions.setPosition(new Vector2(graphics.PreferredBackBufferWidth/2 -225, 400));
             btnShop = new mButton(Content.Load<Texture2D>("Shop_Button"), graphics.GraphicsDevice);
             btnShop.setPosition(new Vector2(graphics.PreferredBackBufferWidth - 250, 0));
+            #endregion
 
+            #region ShopIcons
             //------------Shop Icons-------------------------
             laserIcon1 = new ShopIcon(Content.Load<Texture2D>("ShopIcons\\Icon.1_42"), new Vector2(312, 163), 1, true, 50);
             laserIcon2 = new ShopIcon(Content.Load<Texture2D>("ShopIcons\\Icon.1_43"), new Vector2(312, 219), 1, true, 150);
             laserIcon3 = new ShopIcon(Content.Load<Texture2D>("ShopIcons\\Icon.1_44"), new Vector2(312, 275), 1, true, 300);
             laserIcon4 = new ShopIcon(Content.Load<Texture2D>("ShopIcons\\Icon.1_45"), new Vector2(312, 332), 1, true, 500);
-            //Greyed Out Versions
+
+            shield1 = new ShopIcon(Content.Load<Texture2D>("ShopIcons\\Icon.1_42"), new Vector2(372, 163), 1, true, 50);
+            shield2 = new ShopIcon(Content.Load<Texture2D>("ShopIcons\\Icon.1_42"), new Vector2(372, 219), 1, true, 50);
+            shield3 = new ShopIcon(Content.Load<Texture2D>("ShopIcons\\Icon.1_42"), new Vector2(372, 275), 1, true, 50);
+            shield4 = new ShopIcon(Content.Load<Texture2D>("ShopIcons\\Icon.1_42"), new Vector2(372, 332), 1, true, 50);
+            //----------Greyed Out Versions------------------
             laserIcon1G = new ShopIcon(Content.Load<Texture2D>("ShopIcons\\Icon.1_42_Grey"), new Vector2(312, 163), 1, true, 50);
             laserIcon2G = new ShopIcon(Content.Load<Texture2D>("ShopIcons\\Icon.1_43_Grey"), new Vector2(312, 219), 1, true, 150);
             laserIcon3G = new ShopIcon(Content.Load<Texture2D>("ShopIcons\\Icon.1_44_Grey"), new Vector2(312, 275), 1, true, 300);
             laserIcon4G = new ShopIcon(Content.Load<Texture2D>("ShopIcons\\Icon.1_45_Grey"), new Vector2(312, 332), 1, true, 500);
-
-
-
-            
-            //----------------------------------------------
-
+            #endregion
 
 
             cursor = Content.Load<Texture2D>("cursor");
 
+            #region multiBack
             b = new MultiBackground(GraphicsDevice);
             b.AddLayer(back, 1, 5f);
             b.SetMoveLeftRight();
             b.StartMoving();
+            #endregion
 
-            
         }
 
         /// <summary>
@@ -178,17 +184,14 @@ namespace Final
         {
             MouseState mouse = Mouse.GetState();
             cursorPos = new Vector2(mouse.X -25, mouse.Y -25);
-
-
             float elapsed = (float)gameTime.ElapsedGameTime.TotalMilliseconds;
-
             fireDelay -= elapsed;
 
 
             switch (currentGameState)
             {
-                
-                
+
+                #region ShopUpdate
                 case GameState.Shop:
 
                     b.Update(gameTime);
@@ -197,7 +200,9 @@ namespace Final
 
 
                     break;
+                #endregion
 
+                #region MenuUpdate
                 case GameState.mainMenu:
                     btnPlay.Update(mouse);
                     if (btnPlay.isClicked == true) currentGameState = GameState.playing;
@@ -214,24 +219,22 @@ namespace Final
                     b.Update(gameTime);
 
                     break;
+                #endregion
 
+                #region PlayingUpdate
                 case GameState.playing:
                     if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                     this.Exit();
            
-                    UpdateInput();
+                   
+                    enemy2.Update(gameTime, GraphicsDevice);
                     player.Update(gameTime, GraphicsDevice);
-                    
-                    
-                    
-                  
-
-
+                    enemy1.Update(gameTime);
+                    UpdateInput();
                      for (int i = 0; i < laserList.Count; i++)
                       {
 
                         laserList[i].Update(gameTime);
-
 
                         if (laserList[i].Position.Y < 0f || laserList[i].Position.X < 0f || laserList[i].Position.X > graphics.GraphicsDevice.Viewport.Width
                         || laserList[i].Position.Y > graphics.GraphicsDevice.Viewport.Height)
@@ -244,12 +247,14 @@ namespace Final
             }
 
                     break;
+                #endregion
 
+                #region optionsUpdate
                 case GameState.options:
                     
                     
                     break;
-
+                #endregion
             }
             // Allows the game to exit
             
@@ -268,13 +273,13 @@ namespace Final
            
             switch (currentGameState)
             {
-                
+                #region ShopDraw
                 case GameState.Shop:
 
                     spriteBatch.Begin();
 
                     b.Draw();
-                    particleEngine.Draw(spriteBatch);
+                   // particleEngine.Draw(spriteBatch);
                     gui.Draw(gameTime, spriteBatch);
                    
                    
@@ -282,20 +287,55 @@ namespace Final
                         laserIcon2G.Draw(gameTime, spriteBatch);
                         laserIcon3G.Draw(gameTime, spriteBatch);
                         laserIcon4G.Draw(gameTime, spriteBatch);
-                    
+
+                       /* shield1.Draw(gameTime, spriteBatch);
+                        shield2.Draw(gameTime, spriteBatch);
+                        shield3.Draw(gameTime, spriteBatch);
+                        shield4.Draw(gameTime, spriteBatch);
+                    */
+                        string laser1String = "Laser 1.0";
+                        string laser2String = "Laser 1.5";
+                        string laser3String = "Laser 2.0";
+                        string laser4String = "Laser 2.5";
+                        //string error = "Requires Previous Component"; 
+
+                      
+                        Color fontColor = Color.LightBlue;
+                        MouseState mouse = Mouse.GetState();  
+                        //Vector2 loc = new Vector2(mouse.X, mouse.Y);
+                        //string locstring = loc.ToString();
+                        Rectangle descRect = new Rectangle(300, 425, 200, 35);
+                        Vector2 descLoc = new Vector2(300, 410);
+                        
+                  
+
+                        if (laserIcon1G.CollisionRectangle.Contains(mouse.X, mouse.Y))
+                        {
+                            spriteBatch.DrawString(font, laser1String, descLoc, fontColor);
+                        }
+                        if (laserIcon2G.CollisionRectangle.Contains(mouse.X, mouse.Y))
+                        {
+                            spriteBatch.DrawString(font, laser2String, descLoc, fontColor);
+                        }
+                        if (laserIcon3G.CollisionRectangle.Contains(mouse.X, mouse.Y))
+                        {
+                            spriteBatch.DrawString(font, laser3String, descLoc, fontColor);
+                        }
+                        if (laserIcon4G.CollisionRectangle.Contains(mouse.X, mouse.Y))
+                        {
+                            spriteBatch.DrawString(font, laser4String, descLoc, fontColor);
+                        }
+                        drawError(gameTime);
+                       
                     spriteBatch.Draw(cursor, cursorPos, Color.White);
-
-
                     spriteBatch.End();
 
                   
                     break;
-                
-                
-                
+                #endregion
+
+                #region MenuDraw
                 case GameState.mainMenu:
-                    
-                    
                     
                     spriteBatch.Begin();
                    //spriteBatch.Draw(back, new Rectangle(0, 0, 800, 600), Color.White);
@@ -308,24 +348,19 @@ namespace Final
                   
                     spriteBatch.Draw(cursor, cursorPos, Color.White);
                     
-                 
-                    
-                    
-                    
-                    
-                    
                     spriteBatch.End();
 
-                       
-                   
-
                     break;
+                #endregion
 
+                #region gameDraw
                 case GameState.playing:
 
                     spriteBatch.Begin();
                     spriteBatch.Draw(Content.Load<Texture2D>("Background"), new Rectangle(0, 0, 800, 600), Color.White);
+                    enemy2.Draw(gameTime, spriteBatch);
                     player.Draw(gameTime, spriteBatch);
+                    enemy1.Draw(gameTime, spriteBatch);
                     spriteBatch.Draw(cursor, cursorPos, Color.White);
                     foreach (Sprite lasers in laserList)
                     {
@@ -347,21 +382,18 @@ namespace Final
                     btnShop.Draw(spriteBatch);
                     spriteBatch.End();
                     break;
+                #endregion
 
-
-
+                #region optionsDraw
                 case GameState.options:
 
                     
 
                     break;
+                #endregion
             }
 
           
-
-            
-
-           
             base.Draw(gameTime);
         }
 
@@ -376,6 +408,8 @@ namespace Final
 
             switch (currentGameState)
             {
+                
+                    #region GameInput
                 case GameState.playing:
             MouseState currMouseState = Mouse.GetState();
             if (keyState.IsKeyDown(Keys.Up)
@@ -465,30 +499,23 @@ namespace Final
             {
                 //player.Rotation
                 Vector2 mouseLoc = new Vector2(currMouseState.X, currMouseState.Y);
-
-                
                 Vector2 direction = (player.Position) - mouseLoc;
- 
                 angle = (float)(Math.Atan2(-direction.Y, -direction.X));
-                                
                 player.Rotation = angle + (float)45.5;
                 
-               
-
-
             }
 
              if (keyState.IsKeyDown(Keys.Q))
              {
-                 LaserState = laserState.One;
+                 //LaserState = laserState.One;
              }
              if (keyState.IsKeyDown(Keys.W))
              {
-                 LaserState = laserState.Two;
+                 //LaserState = laserState.Two;
              }
              if (keyState.IsKeyDown(Keys.E))
              {
-                 LaserState = laserState.Three;
+                 enemy1.Velocity = new Vector2(0, 5);
              }
             
 
@@ -528,9 +555,10 @@ namespace Final
             prevKeyState = keyState;
         
               break;
+                #endregion
 
-            
-                
+
+                #region ShopInput
                 case GameState.Shop:
               MouseState currMouseState2 = Mouse.GetState();
               if (currMouseState2.LeftButton == ButtonState.Pressed)
@@ -544,6 +572,7 @@ namespace Final
                       //Console.WriteLine("HITIIIT");
                       laserIcon1G.TextureImage = Content.Load<Texture2D>("ShopIcons\\Icon.1_42");
                       laserIcon1G.active = true;
+                      LaserState = laserState.Two;
                   }
                   
                   if ((laserIcon2G.CollisionMouse(currMouseState2.X, currMouseState2.Y)) && laserIcon1G.active == true)
@@ -551,6 +580,7 @@ namespace Final
                       //Console.WriteLine("HITIIIT");
                       laserIcon2G.TextureImage = Content.Load<Texture2D>("ShopIcons\\Icon.1_43");
                       laserIcon2G.active = true;
+                      LaserState = laserState.Three;
                   }
                   if ((laserIcon3G.CollisionMouse(currMouseState2.X, currMouseState2.Y)) && laserIcon2G.active == true)
                   {
@@ -594,9 +624,10 @@ namespace Final
               prevKeyState = keyState;
 
             break;
-            
-            
-            
+                #endregion
+
+
+
             }//end switch
           
 
@@ -604,10 +635,57 @@ namespace Final
 
         private void spawnEnemies()
         {
+            spawnSmallEnemies();
+            spawnMediumEnemies();
+            spawnBigEnemies();
+
+        }
+        private void spawnSmallEnemies()
+        {
 
 
 
 
+        }
+        private void spawnMediumEnemies()
+        {
+
+
+
+
+        }
+        private void spawnBigEnemies()
+        {
+
+
+
+
+        }
+
+
+        private void drawError(GameTime gameTime)
+        {
+            string error = "Requires Previous \n    Component";
+            Color fontColor = Color.LightBlue;
+            MouseState mouse = Mouse.GetState();
+            Vector2 descLoc = new Vector2(300, 430);
+
+
+
+            if (laserIcon2G.CollisionRectangle.Contains(mouse.X, mouse.Y) && laserIcon1G.active == false)
+            {
+                spriteBatch.DrawString(font, error, descLoc, fontColor);
+            }
+            if (laserIcon3G.CollisionRectangle.Contains(mouse.X, mouse.Y) && laserIcon2G.active == false)
+            {
+                spriteBatch.DrawString(font, error, descLoc, fontColor);
+            }
+            if (laserIcon4G.CollisionRectangle.Contains(mouse.X, mouse.Y) && laserIcon3G.active == false)
+            {
+                spriteBatch.DrawString(font, error, descLoc, fontColor);
+            }
+
+           
         }
 
 
